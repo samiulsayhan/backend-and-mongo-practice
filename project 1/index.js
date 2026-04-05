8f0b75854fcaaa7421d35ea8b8c0442d84c02db3
samiulsayhan <samiulsayhan@gmail.com>
2026-04-06 01:19:11 +0600

    ...

---
+++ project 1/index.js
@@ -0,0 +1,39 @@
+const reload = require("reload");
+const express = require("express");
+const app = express();
+const path = require("path")
+const fs = require('fs')
+
+
+
+app.set("view engine", "ejs"); 
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(function(req,res,next){
+    console.log("aeta run korse")
+    next();
+})
+
+app.get("/",function(req,res){
+    fs.readdir(`./files`,function(err, files){
+        
+        res.render(`index`,{files:files})
+    })
+    
+})
+
+app.post("/create",function(req,res){
+    console.log(req.body);
+    fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`,req.body.details, function(err){
+        res.redirect('/')
+    })
+})
+
+
+app.listen(3000)
+
+reload(app).then((reloadReturned) => {
+
+});
